7a53841c4420625a011a1d69d423423a2fa0cb29
lian <hi@lianmakesthings.dev>
2022-09-13 19:59:12 +0200

    updates website

--- main.go
+++ main.go
@@ -10,19 +10,29 @@ func main() {
 		fmt.Fprintf(w, `
 		<html>
 			<head>
+				<title>DevSpace Demo</title>
 				<link rel="stylesheet" href="https://devspace.sh/css/quickstart.css">
 			</head>
 			<body>
-				<img src="https://devspace.sh/images/congrats.gif" />
-				<h1>You deployed this project with DevSpace!</h1>
-				<div>
-					<h2>Now it's time to code:</h2>
-					<ol>
-						<li>Edit this text in <code>main.go</code> and save the file</li>
-						<li>Restart the application with <code>go run main.go</code></li>
-						<li>Reload browser to see the changes</li>
-					</ol>
-				</div>
+				<section>
+					<div class="container">
+						<div class="left">
+							<h1>You deployed this project with<img class="logo" src="https://static.loft.sh/branding/logos/devspace/horizontal/mono/devspace_horizontal_mono.svg" alt="DevSpace" /></h1>
+							<h2>Now it's time to code 123:</h2>
+							<ol>
+								<ul class="dots">
+									<li class="red"></li>
+									<li class="yellow"></li>
+									<li class="blue"></li>
+								</ul>
+								<li>Edit this text in <code>main.go</code> and save the file</li>
+								<li>Restart the application with <code>go run main.go</code></li>
+								<li>Reload browser to see the changes</li>
+							</ol>
+						</div>
+						<div><img src="https://static.loft.sh/devspace/quickstarts/devspace-astronaut.gif" /></div>
+					</div>
+				</section>
 			</body>
 		</html>
 		`)
